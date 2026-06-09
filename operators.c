5823a6a19739c704940cebd7860b995266a3d45c
Anshu <singhanshu8142@gmail.com>
2026-06-09 11:13:58 +0530

    Add files via upload

---
+++ operators.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(){
+    // int a, b;
+    // printf("Enter two number:");
+    // scanf("%d %d", &a, &b);
+
+    // printf("sum  = %d\n", a+b);
+    // printf("Difference = %d\n", a-b);
+    // printf("Multiply = %d\n", a*b);
+    // printf("Divisin = %d\n", a/b);
+
+    // Assignment Oerators
+    // int num = 10;
+
+    // num+=5;
+    // printf("Sum = %d\n", num);
+
+    // num-=5;
+    // printf("Difference = %d\n", num);
+
+    // num*=5;
+    // printf("Multiply = %d\n", num);
+
+    // num/=5;
+    // printf("Division = %d\n", num);
+
+    // BitWise Operators
+    int a = 5, b = 3;
+    printf("a & b = %d\n", a&b);
+    printf(" a | b = %d\n", a|b);
+    printf("a ^ b = %d", a^b);
+    return 0;
+}
